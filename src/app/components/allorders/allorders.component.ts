cf2f385161b049d6cde9d4cc073f8644a2405745
Yousef Orabi <102566557+youseforabi@users.noreply.github.com>
2024-03-24 17:17:20 +0200

    Update allorders.component.ts

--- src/app/components/allorders/allorders.component.ts
+++ src/app/components/allorders/allorders.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { ProductService } from 'src/app/core/services/product.service';
 
 @Component({
   selector: 'app-allorders',
@@ -8,6 +10,37 @@ import { CommonModule } from '@angular/common';
   templateUrl: './allorders.component.html',
   styleUrls: ['./allorders.component.scss']
 })
-export class AllordersComponent {
+export class AllordersComponent implements OnInit {
+
+  userId:string = '';
+  products :any[]=[];
+  userOrders :any ={};
+  constructor( private _AuthService:AuthService, private _ProductService:ProductService) { }
+
+  ngOnInit(): void {
+      this._AuthService.decodeUser();
+      this.userId = this._AuthService.userId;
+      console.log(this.userId);
+
+
+      this._ProductService.getAllOrders(this.userId).subscribe({
+        next:(res)=>{
+          this.products = res;
+          this.userOrders = this.products[this.products.length -1];
+          console.log(this.userOrders.cartItems);
+          
+          
+        },
+        error:(err)=>{
+          console.log(err);
+          
+        }
+      })
+  }
+  
+
+
+  
 
 }
+
